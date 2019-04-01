63f6ece4f7977d2638adb016a91610537176ffb5
Mandeep Kaur <kaurmandeep239@gmail.com>
2019-04-01 19:48:11 -0400

    Spinner is added to the next screen and coding is done for the same

--- app/src/main/java/com/example/datapersistance/SharedActivity.java
+++ app/src/main/java/com/example/datapersistance/SharedActivity.java
@@ -2,10 +2,15 @@ package com.example.datapersistance;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 import android.widget.Toast;
+import android.widget.ArrayAdapter;
 
 public class SharedActivity extends AppCompatActivity
 {
+    String [] Country = {"India","Canada","Russia","Maxico","Brazil"};
 
 
     @Override
@@ -14,13 +19,31 @@ public class SharedActivity extends AppCompatActivity
         setContentView(R.layout.activity_shared);
 
         Bundle mbundle = getIntent().getExtras();
-        if(mbundle != null)
-        {
-            if(mbundle.containsKey("name"))
-            {
+        if (mbundle != null) {
+            if (mbundle.containsKey("name")) {
                 String name = mbundle.getString("name");
-                Toast.makeText(this,name, Toast.LENGTH_LONG).show();
+                Toast.makeText(this, name, Toast.LENGTH_LONG).show();
             }
         }
+
+         Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, Country);
+        spinner.setAdapter(dataAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                String item = parent.getItemAtPosition(position).toString();
+                Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+            }
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+            }
+
+        });
+
+
     }
 }
